5d66b349c8019c03baa2d4ef6f423b87dabb8482
shayne_coats <shayne_coats@LAPTOP-L6MEN264>
2023-08-02 21:50:52 -0500

    added changes to index.html & taxes.js

--- taxes.js
+++ taxes.js
@@ -7,3 +7,19 @@ function calculateTaxes(income) {
   }
   
   console.log(calculateTaxes(500))
+
+
+// let random = Math.floor(Math.random() * 10)
+// let i = 1;
+// let O = 0;
+// while (i <= 100) {
+//   if (i % 2 === 0) {
+//     if (i === random) {
+//       console.log("oh no");
+//   }
+//   else if (i % 2 !== 0) {
+//     console.log("nah");
+//   }
+//   i++;
+//   }
+// }
